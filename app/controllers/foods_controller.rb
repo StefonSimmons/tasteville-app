4aff418515d70a0e76f9f5fb0b3f6b31b4d59cb8
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2021-08-09 15:09:00 -0400

    full crud on foods

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,44 @@
+class FoodsController < ApplicationController
+  before_action :get_food, only: [:show, :update, :destroy]
+
+  def index
+    foods = Food.all
+    render json: foods
+  end
+
+  def show
+    render json: @food
+  end
+
+  def create
+    food = Food.new(food_params)
+    if food.save
+      render json: food, status: :created
+    else
+      render json: food.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @food.update(food_params)
+      render json: @food, status: :ok
+    else
+      render json: food.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @food.destroy
+    render json: "DELETED"
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :user_id)
+  end
+
+  def get_food
+    @food = Food.find(params[:id])
+  end
+end
