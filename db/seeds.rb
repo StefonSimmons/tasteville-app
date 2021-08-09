763fce890aaaea990ddb30aa0c9f5ad334c10187
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2021-08-09 13:59:40 -0400

    more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Flavor.destroy_all
+Food.destroy_all
+User.destroy_all
+
 flamingo = User.create(username: "flamingo", email: "flamingo@ga.edu", password_digest: "123456")
 jessa = User.create(username: "Jessa", email: "jess@ga.edu", password_digest: "123456")
 
-Food.create(name: "oatmeal", user: flamingo)
-Food.create(name: "captain crunch", user: jessa)
-Food.create(name: "raisin bran", user: jessa)
+pp "#{User.count} users created"
 
+oatmeal = Food.create(name: "oatmeal", user: flamingo)
+bagel = Food.create(name: "bagel", user: flamingo)
+captain_crunch = Food.create(name: "captain crunch", user: jessa)
+raisin_bran = Food.create(name: "raisin bran", user: jessa)
+bonito_flakes = Food.create(name: "bonito flakes", user: flamingo)
+kimchi = Food.create(name: "kimchi", user: flamingo)
+
+pp "#{Food.count} foods created"
 
 spicy = Flavor.create(name: "spicy")
 sweet = Flavor.create(name: "sweet")
@@ -20,4 +30,16 @@ bitter = Flavor.create(name: "bitter")
 sour = Flavor.create(name: "sour")
 salty = Flavor.create(name: "salty")
 umami = Flavor.create(name: "umami")
+
+pp "#{Flavor.count} flavors created"
+
+
+kimchi.flavors.push(umami, spicy, sweet, sour)
+bonito_flakes.flavors.push(umami)
+
+captain_crunch.flavors << sweet
+bagel.flavors << salty
+oatmeal.flavors << sweet
+oatmeal.flavors << spicy
+# oatmeal.flavors.push(sweet, spicy)
 
